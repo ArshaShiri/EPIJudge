8a3161373b81bd563eda5bd1fcce64842b90fb04
Arsha <arsha.shiri@gmail.com>
2022-06-26 16:31:31 +0200

    buy_and_sell_stocks o(n^2) solution.

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -1,13 +1,28 @@
+#include <iostream>
 #include <vector>
 
 #include "test_framework/generic_test.h"
 using std::vector;
-double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+double BuyAndSellStockOnce(const vector<double> &prices) {
+  auto maxProfit = 0.0;
+
+  for (auto buyPriceIdx = 0; buyPriceIdx < prices.size(); ++buyPriceIdx) {
+    const auto buyPrice = prices.at(buyPriceIdx);
+
+    for (auto sellPriceIdx = buyPriceIdx + 1; sellPriceIdx < prices.size();
+         ++sellPriceIdx) {
+      const auto sellPrice = prices.at(sellPriceIdx);
+      const auto profit = sellPrice - buyPrice;
+
+      if (profit > maxProfit)
+        maxProfit = profit;
+    }
+  }
+
+  return maxProfit;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"prices"};
   return GenericTestMain(args, "buy_and_sell_stock.cc",
