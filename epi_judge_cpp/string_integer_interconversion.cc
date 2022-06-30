dd3c90f1540e3a17b84b5f929d356dfcbe86d8c0
Arsha <arsha.shiri@gmail.com>
2022-06-30 16:35:29 +0200

    Added solution to 6_1_string_integer_interconversion

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -4,15 +4,63 @@
 #include "test_framework/test_failure.h"
 using std::string;
 
-string IntToString(int x) {
-  // TODO - you fill in here.
-  return "0";
+// O(n) time and O(1) space
+std::string IntToString(int x) {
+  const auto isNegative = x < 0 ? true : false;
+
+  if (isNegative)
+    x = -x;
+
+  std::string result;
+  const auto zeroChar = '0';
+
+  do {
+    result.push_back(zeroChar + (x % 10));
+    x = x / 10;
+  } while (x != 0);
+
+  if (isNegative)
+    result.push_back('-');
+
+  return {result.rbegin(), result.rend()};
 }
-int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+
+// O(n) time and O(1) space
+int StringToInt1(const std::string &s) {
+  if (s.empty())
+    return 0;
+
+  auto result = 0;
+  auto base = 1;
+
+  const auto zeroChar = '0';
+
+  for (auto rIt = s.rbegin(); rIt != s.rend(); ++rIt) {
+    const auto currentValue = *rIt;
+    if (currentValue == '-') {
+      result = -result;
+      break;
+    }
+
+    if (currentValue == '+') {
+      break;
+    }
+
+    result = result + ((currentValue - zeroChar) * base);
+    base *= 10;
+  }
+
+  return result;
 }
-void Wrapper(int x, const string& s) {
+
+int StringToInt(const std::string &s) {
+  return (s.at(0) == '-' ? -1 : 1) *
+         std::accumulate(
+             s.begin() + ((s.at(0) == '-') || (s.at(0) == '+') ? 1 : 0),
+             s.end(), 0, [](int sum, char c) { return sum * 10 + c - '0'; });
+}
+
+void Wrapper(int x, const string &s) {
   if (stoi(IntToString(x)) != x) {
     throw TestFailure("Int to string conversion failed");
   }
@@ -22,7 +70,7 @@ void Wrapper(int x, const string& s) {
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "s"};
   return GenericTestMain(args, "string_integer_interconversion.cc",
