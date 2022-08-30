c14ff0004d1947984848fbcb4bccb7d7c2c3b62c
Arsha <arsha.shiri@gmail.com>
2022-08-30 20:39:49 +0200

    Added the solution for 9_1_is_tree_balanced

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,11 +1,38 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+
+using IsBalancedAndHeight = std::pair<bool, int>;
+
+IsBalancedAndHeight checkIfBalanced(const BinaryTreeNode<int> *tree) {
+  if (tree == nullptr)
+    return {true, -1};
+
+  const auto leftSide = checkIfBalanced(tree->left.get());
+  if (!leftSide.first)
+    return {false, 0};
+
+  const auto rightSide = checkIfBalanced(tree->right.get());
+  if (!rightSide.first)
+    return {false, 0};
+
+  if (abs(leftSide.second - rightSide.second) > 1)
+    return {false, 0};
+
+  const auto size = std::max(leftSide.second, rightSide.second) + 1;
+  return {true, size};
 }
 
-int main(int argc, char* argv[]) {
+bool IsBalanced(const unique_ptr<BinaryTreeNode<int>> &tree) {
+  if (tree == nullptr)
+    return true;
+
+  if ((tree->left == nullptr) && (tree->right == nullptr))
+    return true;
+
+  return checkIfBalanced(tree.get()).first;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree"};
   return GenericTestMain(args, "is_tree_balanced.cc", "is_tree_balanced.tsv",
