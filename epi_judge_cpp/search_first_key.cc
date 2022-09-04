15f2e48d9dc036e22b769ca1260e8e41c58ec58a
Arsha <arsha.shiri@gmail.com>
2022-09-04 21:03:12 +0200

    Added a solution for 11_1search_first_key

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -3,9 +3,47 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+// Use of STL. Space complexity is O(1)
+// Time complexity is O(logn)
+// If the provided container does not support 
+// random-access iterators, time complexity can be O(n)
+int SearchFirstOfK1(const vector<int>& A, int k) {
+  if (A.empty())
+    return -1;
+
+  const auto valueIt = std::lower_bound(A.begin(), A.end(), k);
+  
+  if ((valueIt == A.end()) || (*valueIt != k))
+    return -1;
+
+  return std::distance(A.begin(), valueIt);
+}
+
+// Time complexity is O(logn)
+int SearchFirstOfK(const std::vector<int>& A, int k) {
+  int idx = -1;
+
+  if (A.empty())
+    return idx;
+
+  int lowIdx = 0;
+  int highIdx = A.size() - 1;
+
+  while (lowIdx <= highIdx) {
+    const auto currentIdx = lowIdx + (highIdx - lowIdx) / 2;
+    const auto currentValue = A.at(currentIdx);
+
+    if (currentValue < k)
+        lowIdx = currentIdx + 1;
+    else {
+        highIdx = currentIdx - 1;
+
+        if (currentValue == k)
+            idx = currentIdx;
+    }
+  }
+
+  return idx;
 }
 
 int main(int argc, char* argv[]) {
